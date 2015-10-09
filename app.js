ae59131d1f1bf18c194a930d73498e6c2d6007c7
Eugene Dryahlov <e.dryahlov@i2013.ru>
2015-10-09 23:54:39 +0400

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+/* jshint browser: true */
+
+window.onload = function() {
+    "use strict";
+    
+    var version = 1.2;
+    document.getElementById("ver").innerHTML = '(ver. '+ String(version)+')';
+    
+    var canvas = document.getElementById("game");
+    var context = canvas.getContext("2d");
+
+    
+};
+
