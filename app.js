d7c4cfc9bef8514a08c40bbe13597330c0b27553
Eugene Dryahlov <e.dryahlov@i2013.ru>
2015-10-11 14:30:37 +0400

    ver 0.9

--- app.js
+++ app.js
@@ -3,27 +3,28 @@
 window.onload = function() {
     "use strict";
     
-    var version = 0.5;
+    var version = 0.9;
     document.getElementById("ver").innerHTML = '(ver. '+ String(version)+')';
     
     var canvas = document.getElementById("game");
     var context = canvas.getContext("2d");
     
     var knock = new Audio('knock.mp3');
+        //knock.volume = 0;
     var glass = new Audio('glass.mp3');
+        //glass.volume = 0;
     var music = new Audio('music.mp3');
-        //music.loop = true;
+        music.loop = true;
         music.currentTime = 6;
-        music.volume=0.9;
+        music.volume=0.2;
     
     var block = {
             height : 20,
             width : 50
     };
-
     var ball = {
-            x : 200,
-            y : 100,
+            x : 350,
+            y : 400,
             rad : 10,
             direction : {
                 x : "none",
@@ -33,25 +34,30 @@ window.onload = function() {
             step : 1
     };
     
-    var url = "shape_level1.txt";
-    var collision = false, collisionWithBlock, collisionWithBorderX = false, collisionWithBorderY = false;
+    var lvl=1;
+    //var url = "shape_level"+lvl+".txt";
+    var collision = false, collisionWithBlock, collisionWithBat = false, collisionWithBorderX = false, collisionWithBorderY = false;
     var tickInterval = ball.speed;
     var safeBorders = 5;
     
-    var blockOrder = getBlockOrder(url);
+    var blockOrder = getBlockOrder();
     var blockCoords = new Array(0);
     
-    var longestLine;
+    var longestLine, mousePos;
     for (var i=0; i<blockOrder.length-1; i++) {
         longestLine = Math.max(blockOrder[i].length,blockOrder[i+1].length);
     }
     c(longestLine);
     canvas.width = block.width * longestLine;
-    canvas.height = block.height * blockOrder.length + 200;
+    canvas.height = block.height * blockOrder.length + 300;
     
-    ball.x = canvas.width / 2;
-    //ball.y = canvas.height - (ball.rad + 5);
-    
+    var bat = {
+            x : canvas.width / 2,
+            y : canvas.height - 10 - block.height
+    };
+
+    ball.x = bat.x / 2;
+    ball.y = bat.y - block.height - (ball.rad + 5);
 
     ///////////////////////////////////////
     ///////////////////////////////////////
@@ -65,19 +71,44 @@ window.onload = function() {
         context.fillRect(0,0,canvas.width,canvas.height);
         
         drawBlocks();
+        drawBat();
         drawBall();
         checkCollision();
         if (collision) {reactOnCollision();}
+        if (blockCoords.length === 0) {changeLevel();}
         moveBall();
     },tickInterval);
-
-
+    
+    document.onmousemove = handleMouseMove;
+    setInterval(getMousePosition, 10);
     
     ///////////////////////////////////////
     ///////////////////////////////////////
     ///////////////////////////////////////
+    
+    function changeLevel() {
+        if (lvl === 6) {
+            lvl++;
+            blockOrder = getBlockOrder();
+            document.getElementById("lvl").innerHTML = lvl;
+            getBlockCoords();
+        } else {
+            alert("Game Over!\n\nWell done!");
+            window.location = "index.html";
+        }
+    }
+    
+    function handleMouseMove(event) {
+        mousePos = {x: event.pageX, y: event.pageY};
+    }
+    function getMousePosition() {
+        if (mousePos.x >= 0 && mousePos.x <= (canvas.width - block.width * 2)) {
+            bat.x = mousePos.x;
+        }
+    }
 
     function checkCollision() {
+        /////////// detect block collision
         for (var i=0; i<blockCoords.length; i++) {
             if (
                 blockCoords[i].coord.x <= (ball.x + ball.rad) && (blockCoords[i].coord.x + block.width) >= (ball.x - ball.rad) &&
@@ -89,6 +120,15 @@ window.onload = function() {
                 break;
             }
         }
+        /////////// detect bat collision
+        if (
+                bat.x <= (ball.x + ball.rad) && (bat.x + block.width * 2) >= (ball.x - ball.rad) &&
+                bat.y < (ball.y + ball.rad)
+            ) {
+                collision = true;
+                collisionWithBat = true;
+        }
+        /////////// detect borders collision
         if (
             (ball.y + ball.rad) > canvas.height - safeBorders || 
             (ball.y - ball.rad) <= 0 + safeBorders
@@ -111,7 +151,6 @@ window.onload = function() {
             context.fillStyle = "red";
             context.fillRect(blockCoords[collisionWithBlock].coord.x, blockCoords[collisionWithBlock].coord.y, block.width, block.height);
             blockCoords.splice(collisionWithBlock,1);
-            //c(collisionWithBlock +', left: '+ blockCoords.length);
             
         }
 
@@ -159,8 +198,10 @@ window.onload = function() {
         }
     }
     
-    function getBlockOrder(url) {
-        var stringData = $.ajax({url: url, async: true}).responseText;
+    function getBlockOrder() {
+        //var urll = document.getElementById("lvl"+lvl).innerHTML;
+        var url = "shape_level"+lvl+".txt";
+        var stringData = $.ajax({url: url, cache: false, async: false}).responseText;
         return stringData.split("\r\n");
     }
     
@@ -169,6 +210,7 @@ window.onload = function() {
             for (var z=0; z<blockOrder[i].length; z++) {
                 var x = z * block.width;
                 var y = i * block.height;
+                var blockType = blockOrder[i][z] || '0';
                 
                 blockCoords[blockCoords.length] = {
                     coord : {
@@ -180,8 +222,7 @@ window.onload = function() {
                         Math.floor(Math.random()*200),
                         Math.floor(Math.random()*200)
                     ],
-                    type : "-",
-                    show : true
+                    type : blockType
                 };
             }
         }
@@ -189,21 +230,23 @@ window.onload = function() {
 
     function drawBlocks() {
         for (var i=0; i<blockCoords.length; i++) {
-            //context.fillStyle = 'rgb('+Math.floor(Math.random()*250)+','+Math.floor(Math.random()*250)+','+Math.floor(Math.random()*250)+')';
-            context.fillStyle = 'rgb('+blockCoords[i].color[0]+','+blockCoords[i].color[1]+','+blockCoords[i].color[2]+')';
-            context.fillRect(blockCoords[i].coord.x, blockCoords[i].coord.y, block.width, block.height);
+            if (blockCoords[i].type === '-') {
+                context.fillStyle = 'rgb('+blockCoords[i].color[0]+','+blockCoords[i].color[1]+','+blockCoords[i].color[2]+')';
+                context.fillRect(blockCoords[i].coord.x, blockCoords[i].coord.y, block.width, block.height);
+            }
         }
     }
 
-    function drawBall(x,y,rad) {
-        x = x || ball.x;
-        y = y || ball.y;
-        rad = rad || ball.rad;
-
+    function drawBall() {
         context.beginPath();
-        context.arc(x,y,rad,0,2*Math.PI);
+        context.arc(ball.x,ball.y,ball.rad,0,2*Math.PI);
         context.fillStyle = "#000";
         context.fill();
+    }
+    
+    function drawBat() {
+        context.fillStyle = "black";
+        context.fillRect(bat.x, bat.y, block.width * 2, block.height);
     }
     
     function c(out) {console.log(out);}
